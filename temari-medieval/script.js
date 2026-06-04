7b61fc000eadcab88f1ccdc30ac7c4276a4598a2
Oxido <iker.bc@gmail.com>
2026-06-04 18:59:05 +0200

    Show source fragments for correct quiz answers

--- temari-medieval/script.js
+++ temari-medieval/script.js
@@ -8,6 +8,83 @@ const conceptTexts = {
   novelles: "Les novel·les medievals tractaven política, cavalleria i amor cortès."
 };
 
+const sourceParagraphs = {
+  v1: "Els Vikings provenien de l'actual Escandinàvia. La seva imatge ha estat deformada pel còmic, el cinema i les òperes de Richard Wagner: no eren només guerrers, també eren navegants, comerciants i creadors de cultura.",
+  v2: "Tenien sagues escrites en rúnic i una religió politeista amb Thor, Odin, Loki i Freya. Políticament, apareixen relacionats amb Normandia, Anglaterra i Sicília.",
+  v3: "Normandia: Rolland i Gisela, acord matrimonial, feudalisme i vassallatge. Anglaterra: 1066, Hastings, Guillem el Conqueridor i tapís de Bayeux. Sicília: mercenaris normands i regne multicultural amb Roger II.",
+  v4: "Es van expandir cap a França, Rússia, Anglaterra, Bretanya i el sud d'Itàlia a partir del canvi climàtic del segle IX. Comerciaven amb productes agrícoles i amb materials com l'ambre rus i la pell de marta.",
+  c1: "El saber no estava només en mans de religiosos: també hi havia autoritats laiques, ciutats, universitats, dones cultes, llengües diferents i cultura oral.",
+  c2: "Entre els segles XI i XII apareixen universitats a ciutats com Bolonya, París, Oxford o Lleida. Usaven el llatí i el mètode de l'escolàstica, basat en el diàleg entre professor i alumne.",
+  c3: "Les novel·les medievals tractaven política, cavalleria i amor cortès. Política: Dante i la “Monarquia”, amb l'objectiu d'aconseguir la pau. Cavalleria: rei Artur, taula rodona i recerca del Grial. Amor: Tristany i Isolda com a exemple d'amor cortès.",
+  c4: "Eren centres religiosos dirigits per monjos i monges. Protegien el coneixement mitjançant traduccions. Figures com Roswitha i Hildegarden de Bingen mostren que també hi havia dones cultes.",
+  a1: "L'amor cortès és un gènere literari del segle XII lligat a Leonor d'Aquitània i a la cultura aristocràtica. Presenta un triangle amorós, un amor prohibit amb secret i tensió, i una dona idealitzada que el cavaller converteix en protagonista.",
+  a2: "L'amor cortès es transmet amb trobadors i joglars: uns componien poemes i els altres els cantaven o recitaven.",
+  a3: "L'amor cortès té l'origen al sud de França i a Aquitània. Les llengües associades al temari són la llengua d'Oc i l'alemany.",
+  cr1: "Les Croades buscaven control social, lluita contra l'infidel i domini de les rutes comercials d'Orient. El 1095, el papa Urbà II impulsa una expedició des del concili de Clermont per ajudar l'imperi Bizantí i conquerir Jerusalem.",
+  cr2: "Matèries primeres d'Orient: or i fusta com a productes valuosos, seda lligada al luxe i al comerç, i alum, fonamental per a les indústries tèxtils.",
+  cr3: "El pelegrinatge cap a Jerusalem va portar reis i reines, com Leonor d'Aquitània. Per protegir els camins es va fundar l'ordre del Temple amb Hugo de Payns.",
+  cr4: "Hi havia motius religiosos, polítics i econòmics. Les Croades van provocar pelegrinatges, ordes de cavallers, intercanvis culturals i també radicalització religiosa.",
+  cr5: "Les Croades van provocar intercanvis culturals i també radicalització religiosa. En aquest context, Ramon Llull apareix com una figura que busca l'enteniment entre religions.",
+  b1: "Bancs: institucions creades per intercanviar moneda i protegir els estalvis de les persones. Fires: espais comercials on s'acostumaven a vendre productes.",
+  b2: "Lletres de canvi: documents que permetien cobrar diners en diferents territoris i facilitaven la mobilitat econòmica.",
+  b3: "Els exemples de bancs més famosos del temari són el banc dels templers i, sobretot, els del Medici.",
+  b4: "Entre els segles XI i XII, les Croades connecten Europa amb Orient i impulsen fires, bancs i nous mecanismes de pagament. El temari ho relaciona amb la literatura de Robin Hood, la religió amb el Purgatori i el creixement del comerç.",
+  g1: "Els gremis eren organitzacions de comerciants i artesans que regulaven oficis i productes.",
+  g2: "Les funcions dels gremis eren formar treballadors, protegir condicions com el salari i vendre productes a fires molt cotitzades.",
+  g3: "Els gremis incloïen comerciants i artesans molt variats: metges, llana, mestres de l'or i també sectors femenins.",
+  g4: "Els gremis encaixen dins la societat urbana medieval perquè organitzen oficis, treball i venda de productes en un món de ciutats i fires.",
+  t1: "Els Vikings eren un poble d'origen escandinau que va expandir-se per Europa entre els segles IX i XIII.",
+  t2: "Els Vikings provenien de l'actual Escandinàvia. La seva imatge ha estat deformada pel còmic, el cinema i les òperes de Richard Wagner: no eren només guerrers, també eren navegants, comerciants i creadors de cultura.",
+  t3: "Els Vikings provenien de l'actual Escandinàvia. La seva imatge ha estat deformada pel còmic, el cinema i les òperes de Richard Wagner: no eren només guerrers, també eren navegants, comerciants i creadors de cultura.",
+  t4: "Es van expandir cap a França, Rússia, Anglaterra, Bretanya i el sud d'Itàlia a partir del canvi climàtic del segle IX. Comerciaven amb productes agrícoles i amb materials com l'ambre rus i la pell de marta.",
+  t5: "Es van expandir cap a França, Rússia, Anglaterra, Bretanya i el sud d'Itàlia a partir del canvi climàtic del segle IX. Comerciaven amb productes agrícoles i amb materials com l'ambre rus i la pell de marta.",
+  t6: "Es van expandir cap a França, Rússia, Anglaterra, Bretanya i el sud d'Itàlia a partir del canvi climàtic del segle IX. Comerciaven amb productes agrícoles i amb materials com l'ambre rus i la pell de marta.",
+  t7: "Tenien sagues escrites en rúnic i una religió politeista amb Thor, Odin, Loki i Freya. Políticament, apareixen relacionats amb Normandia, Anglaterra i Sicília.",
+  t8: "Literatura: sagues escrites en rúnic, com les aventures d'Erik “El Rojo”. Religió: politeisme amb Thor, Odin, Loki, Freya i el Valhalla.",
+  t9: "Literatura: sagues escrites en rúnic, com les aventures d'Erik “El Rojo”. Religió: politeisme amb Thor, Odin, Loki, Freya i el Valhalla.",
+  t10: "Literatura: sagues escrites en rúnic, com les aventures d'Erik “El Rojo”. Religió: politeisme amb Thor, Odin, Loki, Freya i el Valhalla.",
+  t11: "Literatura: sagues escrites en rúnic, com les aventures d'Erik “El Rojo”. Religió: politeisme amb Thor, Odin, Loki, Freya i el Valhalla.",
+  t12: "Literatura: sagues escrites en rúnic, com les aventures d'Erik “El Rojo”. Religió: politeisme amb Thor, Odin, Loki, Freya i el Valhalla.",
+  t13: "Normandia: Rolland i Gisela, acord matrimonial, feudalisme i vassallatge. Anglaterra: 1066, Hastings, Guillem el Conqueridor i tapís de Bayeux. Sicília: mercenaris normands i regne multicultural amb Roger II.",
+  t14: "Normandia: Rolland i Gisela, acord matrimonial, feudalisme i vassallatge. Anglaterra: 1066, Hastings, Guillem el Conqueridor i tapís de Bayeux. Sicília: mercenaris normands i regne multicultural amb Roger II.",
+  t15: "Normandia: Rolland i Gisela, acord matrimonial, feudalisme i vassallatge. Anglaterra: 1066, Hastings, Guillem el Conqueridor i tapís de Bayeux. Sicília: mercenaris normands i regne multicultural amb Roger II.",
+  t16: "Normandia: Rolland i Gisela, acord matrimonial, feudalisme i vassallatge. Anglaterra: 1066, Hastings, Guillem el Conqueridor i tapís de Bayeux. Sicília: mercenaris normands i regne multicultural amb Roger II.",
+  t17: "Normandia: Rolland i Gisela, acord matrimonial, feudalisme i vassallatge. Anglaterra: 1066, Hastings, Guillem el Conqueridor i tapís de Bayeux. Sicília: mercenaris normands i regne multicultural amb Roger II.",
+  t18: "Eren centres religiosos dirigits per monjos i monges. Protegien el coneixement mitjançant traduccions. Figures com Roswitha i Hildegarden de Bingen mostren que també hi havia dones cultes.",
+  t19: "Eren centres religiosos dirigits per monjos i monges. Protegien el coneixement mitjançant traduccions. Figures com Roswitha i Hildegarden de Bingen mostren que també hi havia dones cultes.",
+  t20: "Eren centres religiosos dirigits per monjos i monges. Protegien el coneixement mitjançant traduccions. Figures com Roswitha i Hildegarden de Bingen mostren que també hi havia dones cultes.",
+  t21: "Eren centres religiosos dirigits per monjos i monges. Protegien el coneixement mitjançant traduccions. Figures com Roswitha i Hildegarden de Bingen mostren que també hi havia dones cultes.",
+  t22: "Entre els segles XI i XII apareixen universitats a ciutats com Bolonya, París, Oxford o Lleida. Usaven el llatí i el mètode de l'escolàstica, basat en el diàleg entre professor i alumne.",
+  t23: "Entre els segles XI i XII apareixen universitats a ciutats com Bolonya, París, Oxford o Lleida. Usaven el llatí i el mètode de l'escolàstica, basat en el diàleg entre professor i alumne.",
+  t24: "Apareixen universitats com París, Oxford i Lleida, mentre creixen ciutats, fires i bancs. El temari situa aquest procés entre 1150 i 1300.",
+  t25: "Universitat medieval: llengua, llatí; organització, estudiants segons el seu origen; suport, Església i monarquies; mètode, escolàstica, diàleg entre professor i alumne; exemple, Pere Abelard i Eloïsa.",
+  t26: "Universitat medieval: llengua, llatí; organització, estudiants segons el seu origen; suport, Església i monarquies; mètode, escolàstica, diàleg entre professor i alumne; exemple, Pere Abelard i Eloïsa.",
+  t27: "Universitat medieval: llengua, llatí; organització, estudiants segons el seu origen; suport, Església i monarquies; mètode, escolàstica, diàleg entre professor i alumne; exemple, Pere Abelard i Eloïsa.",
+  t28: "L'escolàstica era el mètode universitari basat en el diàleg entre professor i alumne, i treballava a partir de textos d'Aristòtil.",
+  t29: "Universitat medieval: llengua, llatí; organització, estudiants segons el seu origen; suport, Església i monarquies; mètode, escolàstica, diàleg entre professor i alumne; exemple, Pere Abelard i Eloïsa.",
+  t30: "Les novel·les medievals tractaven política, cavalleria i amor cortès. Política: Dante i la “Monarquia”, amb l'objectiu d'aconseguir la pau. Cavalleria: rei Artur, taula rodona i recerca del Grial. Amor: Tristany i Isolda com a exemple d'amor cortès.",
+  t31: "Les novel·les medievals tractaven política, cavalleria i amor cortès. Política: Dante i la “Monarquia”, amb l'objectiu d'aconseguir la pau. Cavalleria: rei Artur, taula rodona i recerca del Grial. Amor: Tristany i Isolda com a exemple d'amor cortès.",
+  t32: "Les novel·les medievals tractaven política, cavalleria i amor cortès. Política: Dante i la “Monarquia”, amb l'objectiu d'aconseguir la pau. Cavalleria: rei Artur, taula rodona i recerca del Grial. Amor: Tristany i Isolda com a exemple d'amor cortès.",
+  t33: "L'amor cortès és un gènere literari del segle XII lligat a Leonor d'Aquitània i a la cultura aristocràtica.",
+  t34: "L'amor cortès és un gènere literari del segle XII lligat a Leonor d'Aquitània i a la cultura aristocràtica.",
+  t35: "L'amor cortès presenta un triangle amorós, un amor prohibit amb secret i tensió, i una dona idealitzada que el cavaller converteix en protagonista.",
+  t36: "L'amor cortès té l'origen al sud de França i a Aquitània. Les llengües associades al temari són la llengua d'Oc i l'alemany.",
+  t37: "L'amor cortès es transmet amb trobadors i joglars: uns componien poemes i els altres els cantaven o recitaven.",
+  t38: "Les Croades eren expedicions polítiques, militars i comercials organitzades per l'Església entre els segles XI i XIII.",
+  t39: "Les Croades eren expedicions polítiques, militars i comercials organitzades per l'Església entre els segles XI i XIII.",
+  t40: "Les Croades buscaven control social, lluita contra l'infidel i domini de les rutes comercials d'Orient. El 1095, el papa Urbà II impulsa una expedició des del concili de Clermont per ajudar l'imperi Bizantí i conquerir Jerusalem.",
+  t41: "Les Croades buscaven control social, lluita contra l'infidel i domini de les rutes comercials d'Orient. El 1095, el papa Urbà II impulsa una expedició des del concili de Clermont per ajudar l'imperi Bizantí i conquerir Jerusalem.",
+  t42: "Matèries primeres d'Orient: or i fusta com a productes valuosos, seda lligada al luxe i al comerç, i alum, fonamental per a les indústries tèxtils.",
+  t43: "El 1099, els cavallers cristians prenen Jerusalem i funden un regne cristià.",
+  t44: "El pelegrinatge cap a Jerusalem va portar reis i reines, com Leonor d'Aquitània. Per protegir els camins es va fundar l'ordre del Temple amb Hugo de Payns.",
+  t45: "El 1187, Saladino reconquesta Jerusalem després de Hattin.",
+  t46: "Les Croades van portar fruites com l'albercoc, estris com les forquilles, la cura del cos i un negoci important al voltant de la guerra.",
+  t47: "Les Croades van provocar intercanvis culturals i també radicalització religiosa. En aquest context, Ramon Llull apareix com una figura que busca l'enteniment entre religions.",
+  t48: "Bancs: institucions creades per intercanviar moneda i protegir els estalvis de les persones. Fires: espais comercials on s'acostumaven a vendre productes.",
+  t49: "Lletres de canvi: documents que permetien cobrar diners en diferents territoris i facilitaven la mobilitat econòmica.",
+  t50: "Els gremis eren organitzacions de comerciants i artesans que regulaven oficis i productes. Servien per formar, protegir condicions com el salari i vendre productes a fires molt cotitzades."
+};
+
 function saveProgress() {
   localStorage.setItem("temariCompleted", JSON.stringify([...completed]));
   updateProgress();
@@ -74,9 +151,29 @@ document.querySelectorAll(".check-action").forEach((button) => {
     questions.forEach((card) => {
       const selected = card.querySelector("input:checked");
       const isCorrect = selected?.value === card.dataset.answer;
+      const questionName = card.querySelector("input[type='radio']")?.name;
+      const sourceText = questionName ? sourceParagraphs[questionName] : "";
+      const previousSource = card.querySelector(".source-paragraph");
+
+      previousSource?.remove();
       card.classList.remove("correct", "wrong");
       card.classList.add(isCorrect ? "correct" : "wrong");
-      if (isCorrect) correct += 1;
+
+      if (isCorrect) {
+        correct += 1;
+
+        if (sourceText) {
+          const sourceBox = document.createElement("div");
+          const sourceLabel = document.createElement("span");
+          const sourceParagraph = document.createElement("p");
+
+          sourceBox.className = "source-paragraph";
+          sourceLabel.textContent = "Fragment del temari";
+          sourceParagraph.textContent = sourceText;
+          sourceBox.append(sourceLabel, sourceParagraph);
+          card.append(sourceBox);
+        }
+      }
     });
 
     const result = exerciseSet.querySelector(".quiz-result");
@@ -104,6 +201,7 @@ document.getElementById("resetProgress").addEventListener("click", () => {
   completed.clear();
   localStorage.removeItem("temariCompleted");
   document.querySelectorAll(".quiz-card").forEach((card) => card.classList.remove("correct", "wrong"));
+  document.querySelectorAll(".source-paragraph").forEach((source) => source.remove());
   document.querySelectorAll(".quiz-result").forEach((result) => {
     result.textContent = "";
     result.removeAttribute("style");
